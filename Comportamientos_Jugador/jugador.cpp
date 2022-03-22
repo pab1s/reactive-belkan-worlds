a67f9693cb41e4e51858a13dc8fceb4dee4d538f
Pablo <pablolivares1502@gmail.com>
2022-03-23 00:17:49 +0100

    Mapeo vision y mapa auxiliar para lvl>=2
    Falta solucionar el mapeo auxiliar que no sea al norte y empezar con el comportamiento del agente. Mapa de recorrido.

--- Comportamientos_Jugador/jugador.cpp
+++ Comportamientos_Jugador/jugador.cpp
@@ -1,103 +1,194 @@
 #include "../Comportamientos_Jugador/jugador.hpp"
+
 #include <iostream>
 using namespace std;
 
-
-
 Action ComportamientoJugador::think(Sensores sensores) {
+    Action accion = actIDLE;
 
-	Action accion = actIDLE;
+    cout << "Posicion: fila " << sensores.posF << " columna " << sensores.posC
+         << " ";
+    switch (sensores.sentido) {
+        case 0:
+            cout << "Norte" << endl;
+            break;
+        case 1:
+            cout << "Este" << endl;
+            break;
+        case 2:
+            cout << "Sur " << endl;
+            break;
+        case 3:
+            cout << "Oeste" << endl;
+            break;
+    }
+    cout << "Terreno: ";
+    for (int i = 0; i < sensores.terreno.size(); i++)
+        cout << sensores.terreno[i];
+    cout << endl;
 
-	cout << "Posicion: fila " << sensores.posF << " columna " << sensores.posC << " ";
-	switch(sensores.sentido){
-		case 0: cout << "Norte" << endl; break;
-		case 1: cout << "Este" << endl; break;
-		case 2: cout << "Sur " << endl; break;
-		case 3: cout << "Oeste" << endl; break;
-	}
-	cout << "Terreno: ";
-	for (int i=0; i<sensores.terreno.size(); i++)
-		cout << sensores.terreno[i];
-	cout << endl;
+    cout << "Superficie: ";
+    for (int i = 0; i < sensores.superficie.size(); i++)
+        cout << sensores.superficie[i];
+    cout << endl;
 
-	cout << "Superficie: ";
-	for (int i=0; i<sensores.superficie.size(); i++)
-		cout << sensores.superficie[i];
-	cout << endl;
+    cout << "Colisión: " << sensores.colision << endl;
+    cout << "Reset: " << sensores.reset << endl;
+    cout << "Vida: " << sensores.vida << endl;
+    cout << endl;
 
-	cout << "Colisión: " << sensores.colision << endl;
-	cout << "Reset: " << sensores.reset << endl;
-	cout << "Vida: " << sensores.vida << endl;
-	cout << endl;
+    // Establecer la posicion y orientacion anterior
+    switch (ultimaAccion) {
+        case actFORWARD:
+            switch (brujula) {
+                case 0:  // Norte
+                    fil--;
+                    auxFil--;
+                    break;
+                case 1:  // Este
+                    col++;
+                    auxCol++;
+                    break;
+                case 2:  // Sur
+                    fil++;
+                    auxFil++;
+                    break;
+                case 3:  // Oeste
+                    col--;
+                    auxCol--;
+                    break;
+            }
+            break;
+        case actTURN_L:
+            brujula = (brujula + 3) % 4;
+            girar_derecha = (rand() % 2 == 0);
+            break;
+        case actTURN_R:
+            brujula = (brujula + 1) % 4;
+            girar_derecha = (rand() % 2 == 0);
+            break;
+    }
 
-	// Establecer la posicion y orientacion anterior
-	switch (ultimaAccion) {
-		case actFORWARD:
-			switch (brujula) {
-				case 0: // Norte
-				fil--; break;
-				case 1: // Este
-				col++; break;
-				case 2: // Sur
-				fil++; break;
-				case 3: // Oeste
-				col--; break;
-			}
-			break;
-		case actTURN_L:
-			brujula = (brujula+3)%4;
-			girar_derecha = (rand()%2==0);
-			break;
-		case actTURN_R:
-			brujula = (brujula+1)%4;
-			girar_derecha = (rand()%2==0);
-			break;
-	}
+    if ((sensores.terreno[0] == 'G' or sensores.nivel == 0) and !bien_situado) {
+        brujula = sensores.sentido;
+        fil = sensores.posF;
+        col = sensores.posC;
+        bien_situado = true;
+    }
 
-	if (sensores.terreno[0]=='G' and !bien_situado) {
-		brujula = sensores.sentido;
-		fil = sensores.posF;
-		col= sensores.posC;
-		bien_situado = true;
-	}
+    if (bien_situado) {
+        incluirMapa();
+        mapearVision(mapaResultado, sensores.terreno, brujula, fil, col);
+    } else {
+        mapearVision(mapaAux, sensores.terreno, norte, auxFil, auxCol);
+    }
 
-	if (bien_situado) {
-		mapearVision(mapaResultado, sensores);
-	}
+    // Decidir la nueva accion
+    // 1 - Encontrar objetos
+    if (sensores.terreno[0] == 'D' and !zapatillas) {
+        zapatillas = true;
+    }
+    if (sensores.terreno[0] == 'K' and !bikini) {
+        bikini = true;
+    }
+    /*
+    objetivo = encontrarObjetos(sensores.terreno);
+    if (objetivo != ninguno) {
+            accion = sigPaso(objetivo);
+    }
+    */
 
-	// Decidir la nueva accion
-	if ((sensores.terreno[2]=='T' or sensores.terreno[2]=='S' or 
-		sensores.terreno[2]=='G') and (sensores.superficie[2]=='_')) {
-		accion = actFORWARD;
-	} else if (!girar_derecha) {
-		accion = actTURN_L;
-	} else {
-		accion = actTURN_R;
-	}
+    // Permitimos el avance en terreno de gasto base
+    accion = accionSimple(sensores);
 
-	// Determinar el efecto de la ultima accion enviada
-	ultimaAccion = accion;
-	return accion;
+    // Determinar el efecto de la ultima accion enviada
+    ultimaAccion = accion;
+    return accion;
 }
 
-int ComportamientoJugador::interact(Action accion, int valor){
-  return false;
+int ComportamientoJugador::interact(Action accion, int valor) { return false; }
+
+void ComportamientoJugador::mapearVision(vector<vector<unsigned char>>& mapa,
+                                         vector<unsigned char> vision,
+                                         int orientacion, int f, int c) {
+    int n = 0, aux = 0, x = 0, y = 0;
+
+    for (int i = 0; i >= -VISION_DEPTH; i--) {
+        for (int j = i; j <= -i; j++) {
+            x = i;
+            y = j;
+            for (int k = 0; k < orientacion; k++) {
+                aux = (-1) * x;
+                x = y;
+                y = aux;
+            }
+            mapa[f + x][c + y] = vision[n];
+            n++;
+        }
+    }
 }
 
-void ComportamientoJugador::mapearVision(vector<vector<unsigned char> >& mapaResultado, Sensores sensores) {
-	int n = 0, aux = 0, x = 0, y = 0;
+int ComportamientoJugador::encontrarObjeto(vector<unsigned char> vision) {
+    int i = 0, pos = UNKNOWN;
+    bool encontrado = false;
 
-	for (int i=0; i >= -VISION_DEPTH; i--) {
-		for (int j = i; j <= -i; j++) {
-			x = i;
-			y = j;
-			for (int k = 0; k < brujula; k++) {
-				aux = (-1)*x;
-				x = y;
-				y = aux;
-			}
-			mapaResultado[fil+x][col+y] = sensores.terreno[n];
-			n++;
-		}
-	}	
+    if (!zapatillas and !bikini) {
+        while (!encontrado and i < vision.size()) {
+            if (vision[i] == 'D' and !zapatillas) {
+                encontrado = true;
+            } else if (vision[i] == 'K' and !bikini) {
+                encontrado = true;
+            }
+            i++;
+        }
+    }
+    pos = encontrado ? --i : UNKNOWN;
+    return pos;
+}
+
+/*
+Action ComportamientoJugador::sigPaso(int dest) {
+        Action paso = actIDLE;
+        int n = 0, x = 0;
+        bool terminado = false;
+
+        while (n <= VISION_DEPTH and !terminado) {
+                x = x + 2*n;
+                if (dest == x or ) {
+                        paso = actFORWARD;
+                } else if (dest <= (x+n)) {
+
+                }
+        }
+}
+*/
+
+Action ComportamientoJugador::accionSimple(Sensores sensores) {
+    Action paso = actIDLE;
+
+    if ((sensores.terreno[2] == 'T' or sensores.terreno[2] == 'S' or
+         sensores.terreno[2] == 'G' or sensores.terreno[2] == 'X' or
+         sensores.terreno[2] == 'D' or sensores.terreno[2] == 'K' or
+         (sensores.terreno[2] == 'B' and zapatillas) or
+         (sensores.terreno[2] == 'A' and bikini)) and
+        (sensores.superficie[2] == '_')) {
+        paso = actFORWARD;
+    } else if (!girar_derecha) {
+        paso = actTURN_L;
+    } else {
+        paso = actTURN_R;
+    }
+
+    return paso;
+}
+
+void ComportamientoJugador::incluirMapa() {
+    for (int i = 0; i < 2 * MAX; i++) {
+        for (int j = 0; j < 2 * MAX; j++) {
+            if (mapaAux[i][j] != '?') {
+                mapaResultado[fil + (i - auxFil)][col + (j - auxCol)] =
+                    mapaAux[i][j];
+            }
+        }
+    }
 }
--- Comportamientos_Jugador/jugador.hpp
+++ Comportamientos_Jugador/jugador.hpp
@@ -1,38 +1,60 @@
 #ifndef COMPORTAMIENTOJUGADOR_H
 #define COMPORTAMIENTOJUGADOR_H
 
-#include "comportamientos/comportamiento.hpp"
 #include <vector>
+
+#include "comportamientos/comportamiento.hpp"
 using namespace std;
 
-class ComportamientoJugador : public Comportamiento{
+class ComportamientoJugador : public Comportamiento {
+   public:
+    ComportamientoJugador(unsigned int size) : Comportamiento(size) {
+        // Constructor de la clase
+        fil = col = auxFil = auxCol = 99;
+        brujula = norte;
+        girar_derecha = false;
+        bien_situado = false;
+        zapatillas = bikini = false;
+        ultimaAccion = actIDLE;
+        vector<unsigned char> vacio(2 * MAX, '?');
 
-  public:
-    ComportamientoJugador(unsigned int size) : Comportamiento(size){
-      // Constructor de la clase
-      fil = col = 99;
-      brujula = 0;
-      girar_derecha = false;
-      bien_situado = false;
-      ultimaAccion = actIDLE;
-      
-      // Dar el valor inicial a las variables de estado
+        for (unsigned int i = 0; i < 2 * MAX; i++) {
+            mapaAux.push_back(vacio);
+        }
+        // Dar el valor inicial a las variables de estado
     }
 
-    ComportamientoJugador(const ComportamientoJugador & comport) : Comportamiento(comport){}
-    ~ComportamientoJugador(){}
+    ComportamientoJugador(const ComportamientoJugador& comport)
+        : Comportamiento(comport) {}
+    ~ComportamientoJugador() {}
 
     Action think(Sensores sensores);
     int interact(Action accion, int valor);
-    void mapearVision(vector<vector<unsigned char> >& mapaResultado, Sensores sensores);
 
-  private:
-  
-  // Declarar aquí las variables de estado
-  const int VISION_DEPTH=2;
-  int fil, col, brujula;
-  bool girar_derecha, bien_situado;
-  Action ultimaAccion;
+   private:
+    // Declarar aquí las variables de estado
+    static const int VISION_DEPTH = 3, UNKNOWN = -1, MAX = 100;
+    int fil, col, auxFil, auxCol, brujula;
+    bool girar_derecha, bien_situado, zapatillas, bikini;
+    Action ultimaAccion;
+    vector<vector<unsigned char>> mapaAux;
+
+    // Métodos privados
+
+    // Inserta en el mapa indicado todo lo que el jugador ve
+    void mapearVision(vector<vector<unsigned char>>& mapa,
+                      vector<unsigned char> vision, int orientacion, int f,
+                      int c);
+
+    // Incluye lo descubierto del mapa origen en el map destino
+    void incluirMapa();
+
+    // Devuelve el objeto más cercano que no dispone el jugador
+    int encontrarObjeto(vector<unsigned char> vision);
+
+    // Devuelve los datos para ir al destino indicado
+    // Action sigPaso(int dest);
+    Action accionSimple(Sensores sensores);
 };
 
 #endif
